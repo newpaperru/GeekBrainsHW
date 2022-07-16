dc6bf8ec0c85082cd6d9fafbc327a226f788f3f0
Boris <boryanski213@gmail.com>
2022-07-16 21:14:19 +0300

    Добавил решение 19 задвчи

---
+++ hw33/hw33.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите 5-ти значное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+// // // последняя цифра числа
+int lastDig = num % 10;
+// предпоследняя цифра числа
+int penDig = num % 100;
+int penDigRes = penDig / 10;
+// вторая цифра числа
+int secDig = num % 10000;
+int secDigRes = secDig / 1000;
+// первая цифра числа
+int firstDig = num / 10000;
+
+if (num > 99999 || num < 10000){
+    Console.WriteLine("Введите корректное число");
+} else if (firstDig == lastDig && penDigRes == secDigRes) {
+    Console.WriteLine("Данное число является палиндромом");
+} else {
+    Console.WriteLine("Данное число не является палиндромом");
+}
